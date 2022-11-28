f04f986ec8bc633bc31f4a21b421d126d0d224b3
lens <okorelens27@gmail.com>
2022-11-28 15:46:57 +0300

    Initial project file

---
+++ test/Inbox.test.js
@@ -0,0 +1,55 @@
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3"); //instace of web3
+const { interface, bytecode } = require("../compile");
+
+const web3 = new Web3(ganache.provider());
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+  //Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  //Use one of those accounts to deploy contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there!"],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
+});
+
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    console.log(inbox);
+  });
+});
+
+// /* Mocha Test */
+
+// class Car {
+//   park() {
+//     return "stopped";
+//   }
+
+//   drive() {
+//     return "vroom";
+//   }
+// }
+
+// let car;
+
+// beforeEach(() => {
+//   car = new Car();
+// });
+
+// describe("Car", () => {
+//   it("can park", () => {
+//     assert.equal(car.park(), "stopped");
+//   });
+
+//   it("can drive", () => {
+//     assert.equal(car.drive(), "vroom");
+//   });
+// })
